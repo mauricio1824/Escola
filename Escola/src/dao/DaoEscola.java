8e3c5e7b8d526f17c9552de7022646bba492b0fc
Administrador <Administrador@10.10.4.4>
2019-08-08 16:48:53 -0300

    Versão 1

---
+++ Escola/src/dao/DaoEscola.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modelo.Escola;
+
+public class DaoEscola {
+    public static boolean inserir(Escola objeto) {
+        String sql = "INSERT INTO pais (nome, endereco, sigla, n_alunos, area) VALUES (?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setString(1, objeto.getNome());
+            ps.setString(2, objeto.getEndereco());
+            ps.setString(3, objeto.getSigla());
+            ps.setString(4, objeto.getN_alunos());
+            ps.setString(5, objeto.getArea());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        Escola objeto = new Escola();
+        objeto.setNome("BR");
+        objeto.setEndereco("Brasil");
+        
+        boolean resultado = inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+}
---
+++ Escola/src/modelo/Escola.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Escola {
+    private int codigo;
+    private String nome;
+    private String endereco;
+    private String sigla;
+    private String n_alunos;
+    private String area;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public String getN_alunos() {
+        return n_alunos;
+    }
+
+    public void setN_alunos(String n_alunos) {
+        this.n_alunos = n_alunos;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
